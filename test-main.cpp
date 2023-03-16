296af6506514522c67594b70425f3dfef55ce730
Jacob Cohen <jacob@jacobcohen.info>
2023-03-16 10:36:47 -0500

    Added iterative collision-finder!
    Then the bad news, the strings "!^M" and "!^L" both have the same hash output. I will keep trying to better my algorithm.
    
    Adding multiple passes did help a lot, though, as it took 14709 strings to find the first collision, compared to about 5 tries by me typing in strings off the top of my head.
    
    A true random oracle (obviously not possible on a computer) should take ~1152921500000000000, though... (by the Birthday Problem). My approach is admittedly different to a standard birthday attack, however, as my strings are NOT generated with any intent at randomness.

--- test-main.cpp
+++ test-main.cpp
@@ -1,8 +1,13 @@
 #include "hashing.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <iostream>
 #include <csignal>
+#include <cstdlib>
+#include <unordered_map>
+
+#define insert(map, k, v) map.insert(std::make_pair<std::string, std::string>(k, v))
 
 using std::cout;
 
@@ -13,21 +18,28 @@ using std::cout;
 
 char* toHex(const char* s, size_t len);
 void interrupt(int signum);
+void doInteractive();
+void __strGenRecHelper(std::unordered_map<std::string, std::string>& map, std::string prefix, uint8_t len);
 
-int main(void){
+int main(int argc, char* argv[]){
     signal(SIGINT, interrupt);
-    puts(START_MSG);
 
+
+    if(argc > 1 && !strcmp(argv[1], "-i")){
+        doInteractive();
+        return 0;
+    }
+
+    puts("Generating all strings of length 1-10 (with printable ASCII characters) and hashing them...");
+    std::unordered_map<std::string, std::string> map; // Storing hashes as strings for easy heap allocation
     char hashed[16] = {'\0'};
-
-    for(std::string s; getline(std::cin, s);){
-        Hashing::hash(hashed, s.c_str(), s.length());
-        char* hex;
-        printf("Hash of \"%s\": %s\n", s.c_str(), hex = toHex(hashed, 15));
-        delete[] hex;
+    // Generate all strings of length 1-4
+    for(uint8_t len = 0; len < 4; len++){
+        __strGenRecHelper(map, "", len);
     }
     
-    puts("Exiting...");
+
+    srand(time(NULL));
 
     return 0;
 }
@@ -43,4 +55,45 @@ void interrupt(int signum){
     // Clear the ^C, print exiting message, and exit.
     puts("\b\b  \rInterrupt signal received. Exiting...");
     exit(0);
+}
+
+void doInteractive(){
+    puts(START_MSG);
+
+    char hashed[16] = {'\0'};
+
+    for(std::string s; getline(std::cin, s);){
+        Hashing::hash(hashed, s.c_str(), s.length());
+        char* hex;
+        printf("Hash of \"%s\": %s\n", s.c_str(), hex = toHex(hashed, 15));
+        delete[] hex;
+    }
+    
+    puts("Exiting...");
+}
+
+void __strGenRecHelper(std::unordered_map<std::string, std::string>& map, std::string prefix, uint8_t len){
+    if(len == 0){
+        char hashed[16] = {'\0'};
+        Hashing::hash(hashed, prefix.c_str(), prefix.length());
+        std::string hashStr(hashed, 15);
+        
+        // Check if hash already exists in map, and if so, print out the collision generating input strings.
+        if(map.find(hashStr) != map.end()){
+            char* hashHex = toHex(hashed, 15);
+            printf("Collision found after %lu elements! Input strings \"%s\" and \"%s\" both hash to (hex bytes) 0x%s\n", map.size(), map[hashStr].c_str(), prefix.c_str(), hashHex);
+            delete[] hashHex;
+            exit(0);
+        }
+
+        insert(map, static_cast<std::string>(hashStr), std::string(prefix));
+        return;
+    }
+
+    // ASCII printable characters range from 33-126 inclusive.
+    for (uint16_t i = 33; i <= 126; i++){
+        std::string newPrefix = prefix + (char)(i);
+
+        __strGenRecHelper(map, newPrefix, len - 1);
+    }
 }
