465fc7786ea879e3f146faf3db8299d7544dc915
Jacob Cohen <jacob@jacobcohen.info>
2023-06-01 15:48:08 -0500

    Switched all-static class to namespace with
    anonymous child namespace to protect previously `private` members

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,54 +1,64 @@
 #include "hashing.h"
 
-// Using a non-zero initial state to add some sort of a "salt"
-void Hashing::clearState(){
-    memcpy((void*)state, (const void*)&stateDefault, STATE_BUF_LEN);
-}
+namespace Hashing {
 
-void Hashing::hash(char output[STATE_BUF_LEN], const char* input, const int length){
-    clearState();
+    namespace { // Anonymous namespace to hide these variables and functions from the end-user
 
-    uint32_t newLength = 0;
-    unsigned char* paddedInput = padInput(input, length, newLength);
+        unsigned char state[STATE_BUF_LEN];
+        uint8_t COMPRESSION_SHIFTS[5] = {5, 3, 5, 3, 5};
 
-    unsigned char lbyte = 0x57; // Initalized to 0x57 to start, why not.
-    for(uint8_t p = 0; p < PASSES; p++){
-        
-        for(uint32_t i = 0; i < newLength; i++){
-            unsigned char cbyte = paddedInput[i];
+        uint8_t stateDefault[STATE_BUF_LEN] = {0xBA, 0x23, 0x8C, 0x4E, 0x38, 0x4D, 0xC1, 0xCA, 0xA1, 0xA4, 0x78, 0xFF, 0x5F, 0xCE, 0xF7};
 
-            // Overflows in the state-buffer are INTENTIONAL to make reversal SLIGHTLY more difficult
-            state[RING_BUF_INDEX(i)] += (cbyte ^ (lbyte)) + *(&cbyte + 1);
+        void clearState(){
+            memcpy((void*)state, (const void*)&stateDefault, STATE_BUF_LEN);
+        }
 
-            // Below lines are INSPIRED by MD6's compression function as listed in the MIT paper: https://people.csail.mit.edu/rivest/pubs/RABCx08.pdf
-            state[RING_BUF_INDEX(i)] ^= (state[RING_BUF_INDEX(i - 1)] ^ state[RING_BUF_INDEX(i - 2)]) ^ state[RING_BUF_INDEX(i - 3)];
+        unsigned char* padInput(const char* input, const uint32_t length, uint32_t& newLength){
+            uint32_t paddedLength = 512 - (length % 512);
 
-            state[RING_BUF_INDEX(i)] ^= (state[RING_BUF_INDEX(i)] >> COMPRESSION_SHIFTS[(uint8_t)((cbyte) & 0xF % 5)]);
-            // state[RING_BUF_INDEX(i)] ^= ((state[RING_BUF_INDEX(i)] >> COMPRESSION_SHIFTS[(uint8_t)((cbyte) & 0xF % 5)])) | (state[RING_BUF_INDEX(i)] << (8 - COMPRESSION_SHIFTS[(uint8_t)((cbyte) & 0xF % 5)]));
-                                                            // Grab least significantt 4 bits from cbyte, then modulo 5 to index the shift array (no zero shifts!).
-                                                            // XOR the current state byte with the shifted state byte to add more pseudo-randomness.
-            lbyte = state[RING_BUF_INDEX(i)]; // Pushes back the first found collision by 10,000. But it is still at 3 characters. 4 characters is the next milestone!
+            if(!paddedLength) // No need to pad if already a multiple of 512
+                return (unsigned char*)input;
+            
+            unsigned char* paddedInput = new unsigned char[newLength = (length + paddedLength)];
+
+            memcpy(paddedInput, input, length);
+
+            // Pad with zeroes, finishing with the original length to prevent collisions
+            memset(paddedInput + length, 0x00, paddedLength);
+            memcpy(paddedInput + length + paddedLength - 4, &length, 4);
+
+            return paddedInput;
         }
     }
 
-    delete[] paddedInput;
+    void hash(char output[STATE_BUF_LEN], const char* input, const int length){
+        clearState();
 
-    memcpy(output, state, STATE_BUF_LEN);
-}
+        uint32_t newLength = 0;
+        unsigned char* paddedInput = padInput(input, length, newLength);
 
-unsigned char* Hashing::padInput(const char* input, const uint32_t length, uint32_t& newLength){
-    uint32_t paddedLength = 512 - (length % 512);
+        unsigned char lbyte = 0x57; // Initalized to 0x57 to start, why not.
+        for(uint8_t p = 0; p < PASSES; p++){
+            
+            for(uint32_t i = 0; i < newLength; i++){
+                unsigned char cbyte = paddedInput[i];
 
-    if(!paddedLength) // No need to pad if already a multiple of 512
-        return (unsigned char*)input;
-    
-    unsigned char* paddedInput = new unsigned char[newLength = (length + paddedLength)];
+                // Overflows in the state-buffer are INTENTIONAL to make reversal SLIGHTLY more difficult
+                state[RING_BUF_INDEX(i)] += (cbyte ^ (lbyte)) + *(&cbyte + 1);
 
-    memcpy(paddedInput, input, length);
+                // Below lines are INSPIRED by MD6's compression function as listed in the MIT paper: https://people.csail.mit.edu/rivest/pubs/RABCx08.pdf
+                state[RING_BUF_INDEX(i)] ^= (state[RING_BUF_INDEX(i - 1)] ^ state[RING_BUF_INDEX(i - 2)]) ^ state[RING_BUF_INDEX(i - 3)];
 
-    // Pad with zeroes, finishing with the original length to prevent collisions
-    memset(paddedInput + length, 0x00, paddedLength);
-    memcpy(paddedInput + length + paddedLength - 4, &length, 4);
+                state[RING_BUF_INDEX(i)] ^= (state[RING_BUF_INDEX(i)] >> COMPRESSION_SHIFTS[(uint8_t)((cbyte) & 0xF % 5)]);
+                // state[RING_BUF_INDEX(i)] ^= ((state[RING_BUF_INDEX(i)] >> COMPRESSION_SHIFTS[(uint8_t)((cbyte) & 0xF % 5)])) | (state[RING_BUF_INDEX(i)] << (8 - COMPRESSION_SHIFTS[(uint8_t)((cbyte) & 0xF % 5)]));
+                                                                // Grab least significantt 4 bits from cbyte, then modulo 5 to index the shift array (no zero shifts!).
+                                                                // XOR the current state byte with the shifted state byte to add more pseudo-randomness.
+                lbyte = state[RING_BUF_INDEX(i)]; // Pushes back the first found collision by 10,000. But it is still at 3 characters. 4 characters is the next milestone!
+            }
+        }
 
-    return paddedInput;
-}
+        delete[] paddedInput;
+
+        memcpy(output, state, STATE_BUF_LEN);
+    }
+};
--- hashing.h
+++ hashing.h
@@ -2,27 +2,10 @@
 #include <cstdio>
 #include <cstdint>
 
-#define STATE_BUF_LEN 15
-#define PASSES 16
-#define RING_BUF_INDEX(i) (signed int)(i) % STATE_BUF_LEN < 0 ? (signed int)(i) % STATE_BUF_LEN + STATE_BUF_LEN : (signed int)(i) % STATE_BUF_LEN
+namespace Hashing {
+    #define STATE_BUF_LEN 15
+    #define PASSES 16
+    #define RING_BUF_INDEX(i) (signed int)(i) % STATE_BUF_LEN < 0 ? (signed int)(i) % STATE_BUF_LEN + STATE_BUF_LEN : (signed int)(i) % STATE_BUF_LEN
 
-typedef unsigned __int128 uint128_t; // This did not work the way I wanted, so I am storing the default state value in a char buffer instead.
-
-class Hashing {
-    private:
-
-    inline static unsigned char state[STATE_BUF_LEN];
-    inline static uint8_t COMPRESSION_SHIFTS[5] = {5, 3, 5, 3, 5};
-
-    // Hex: 0xBA238C4E384DC1CAA1A478FF5FCEF7
-    inline static uint8_t stateDefault[STATE_BUF_LEN] = {0xBA, 0x23, 0x8C, 0x4E, 0x38, 0x4D, 0xC1, 0xCA, 0xA1, 0xA4, 0x78, 0xFF, 0x5F, 0xCE, 0xF7};
-
-    static void clearState();
-
-    // Must be freed by the caller
-    static unsigned char* padInput(const char* input, const uint32_t length, uint32_t& newLength);
-
-    public:
-
-    static void hash(char output[STATE_BUF_LEN], const char* input, const int length);
-};
+    void hash(char output[STATE_BUF_LEN], const char* input, const int length);
+}
